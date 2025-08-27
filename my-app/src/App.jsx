60e828639039a1910ab3ace6285b295cd20a94c3
Bansi Chudasama <bansi1705@gmail.com>
2025-08-27 14:22:42 +0530

    [BC][Cark competed]

---
+++ my-app/src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+import Box from "./components/Box.jsx";
+function App() {
+  return (
+    <>
+      <div className="main">
+        <Box name="Bansi" title="Chudasama" />
+        <Box name="Mirali" title="Vadhiya" />
+        <Box name="Mayuri" title="Kodavala" />
+        <Box name="Hansa" title="Rathod" />
+        <Box name="Vrunda" title="Chudasama" />
+        <Box name="Jyot" title="Chudasama" />
+        <Box name="Harsha" title="chudasama" />
+      </div>
+    </>
+  );
+}
+
+export default App;
